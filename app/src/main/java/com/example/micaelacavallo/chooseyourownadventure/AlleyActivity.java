ac709d3d3a6ba12999ca92d535e038ec7a7dd994
micaela.cavallo <micaela.cavallo@globant.com>
2015-01-23 19:55:33 -0300

    As a user, I want to see the text “You have ventured down a dark alley where would you like to go next”.
    As a user, I want to see a “Go Left” button on the Alley screen.
    
    As a user, I want to see a “Continue” button on the Alley screen.
    
    As a user, I want to see a “Go Right” button on the Alley screen.

---
+++ app/src/main/java/com/example/micaelacavallo/chooseyourownadventure/AlleyActivity.java
@@ -0,0 +1,15 @@
+package com.example.micaelacavallo.chooseyourownadventure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by micaela.cavallo on 23/01/2015.
+ */
+public class AlleyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alley_activity);
+    }
+}
---
+++ app/src/main/java/com/example/micaelacavallo/chooseyourownadventure/StartFragment.java
@@ -0,0 +1,64 @@
+package com.example.micaelacavallo.chooseyourownadventure;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StartFragment extends Fragment {
+    Button mStartTravel;
+
+
+
+    public StartFragment() {
+        // Required empty public constructor
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        prepareStart();
+
+    }
+
+    private void prepareStart() {
+        mStartTravel = (Button)getView().findViewById(R.id.button_start_traveling);
+        mStartTravel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomRoom = new Random();
+                int randomNumber = randomRoom.nextInt(2);
+                if (randomNumber == 0) {
+                    Intent iAlley= new Intent(getActivity(), AlleyActivity.class);
+                    startActivity(iAlley);
+                }
+                else
+                {
+                    Intent iRoom = new Intent(getActivity(), RoomActivity.class);
+                    startActivity(iRoom);
+                }
+            }
+        });
+    }
+
+
+}
