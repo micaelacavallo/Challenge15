3ae4a34c7d64fd7ec83e7b698b643fc6bf7e7388
micaela.cavallo <micaela.cavallo@globant.com>
2015-01-26 11:13:43 -0300

    As a user, I want to see a Room screen with the text “You have entered a room with two doors”
    As a user, I want to see a “Enter Door 1” button and an “Enter Door 2” button

---
+++ app/src/main/java/com/example/micaelacavallo/chooseyourownadventure/FinalFragment.java
@@ -0,0 +1,25 @@
+package com.example.micaelacavallo.chooseyourownadventure;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FinalFragment extends Fragment {
+
+    public FinalFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.final_fragment, container, false);
+    }
+
+
+
+}
